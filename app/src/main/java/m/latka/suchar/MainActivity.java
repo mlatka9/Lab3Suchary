f00ea11328ce4d14abe527de9409ca8ead53cac2
Mateusz Łątka <mateusz.latka9@gmail.com>
2020-10-17 12:59:59 +0200

    update function draw

--- app/src/main/java/m/latka/suchar/MainActivity.java
+++ app/src/main/java/m/latka/suchar/MainActivity.java
@@ -13,17 +13,11 @@ public class MainActivity extends AppCompatActivity {
 
     List<String> jokesList = new ArrayList<>();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void hej(View view) {
-
-        int randomJokeNumber = (int)(Math.random()*jokesList.size());
-
-        TextView textView = findViewById(R.id.textView);
 
         jokesList.add(getResources().getString(R.string.joke1));
         jokesList.add(getResources().getString(R.string.joke2));
@@ -31,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
         jokesList.add(getResources().getString(R.string.joke4));
         jokesList.add(getResources().getString(R.string.joke5));
 
+    }
+
+    public void draw(View view) {
+
+        int randomJokeNumber = (int)(Math.random()*jokesList.size());
+        TextView textView = findViewById(R.id.textView);
         textView.setText(jokesList.get(randomJokeNumber));
     }
 
